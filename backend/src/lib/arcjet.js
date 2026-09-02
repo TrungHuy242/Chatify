d114810b9a3798d9a0aac36f24d4edb741ee0e9b
Huy Trung <huytrung0102@gmail.com>
2026-09-02 15:39:35 +0700

    Fix Arcjet bot detection blocking requests

--- backend/src/lib/arcjet.js
+++ backend/src/lib/arcjet.js
@@ -1,5 +1,6 @@
 import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
-import {ENV} from "./env.js";
+
+import { ENV } from "./env.js";
 
 const aj = arcjet({
   key: ENV.ARCJET_KEY,
@@ -8,21 +9,17 @@ const aj = arcjet({
     shield({ mode: "LIVE" }),
     // Create a bot detection rule
     detectBot({
-      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+      mode: "DRY_RUN ", // Blocks requests. Use "DRY_RUN" to log only
       // Block all bots except the following
       allow: [
         "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        // Uncomment to allow these other common bot categories
-        // See the full list at https://arcjet.com/bot-list
-        //"CATEGORY:MONITOR", // Uptime monitoring services
-        //"CATEGORY:PREVIEW", // Link previews e.g. Slack, Discord
       ],
     }),
     // Create a token bucket rate limit. Other algorithms are supported.
     slidingWindow({
-        mode: "LIVE", 
-        max: 100, 
-        interval: 60, 
+      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+      max: 100,
+      interval: 60,
     }),
   ],
 });
